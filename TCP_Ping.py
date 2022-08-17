b7c5913233489bc2d7a9cd8939d9b482d82f7f24
Baris Berk ZORBA <bbzorba@hotmail.com>
2022-08-17 16:19:57 +0200

    Update TCP_Ping.py

--- TCP_Ping.py
+++ TCP_Ping.py
@@ -2,7 +2,8 @@ from tcp_latency import measure_latency
 
 while(True):    
     try:
-        ping = measure_latency(host='81.95.107.155', port=50061, timeout=2.5)
+        #write here the server's IP address , the port to connect and the timeout.
+        ping = measure_latency(host='192.168.1.2', port=50061, timeout=2.5)
         print(ping, "ms")
     except KeyboardInterrupt:
         break
